8a5b6553ea3b3e20dfe576479bf28e698f6f0227
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-05-24 17:54:06 -0400

    updated css

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import "./weather.css";
 
 export default function Search(props) {
   const [message, setMessage] = useState(null);
