ec8caad8d73b476c09ab41ee26ca5e50cd80bedf
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-05-28 16:59:30 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import Search from "./weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <h1>Weather App</h1>
         <Search />
         <a href="https://github.com/lfinkbiner/react-weather">GitHub Repository</a>
       </header>
+      </div>
     </div>
   );
 }
