c7f4a83079bd9a78a8fae552d0c9f1af44596f76
lfinkbiner <54183577+lfinkbiner@users.noreply.github.com>
2021-06-06 17:20:29 -0400

    implemented unit conversion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import Search from "./weather";
+import Weather from "./weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Search />
+        <Weather defaultCity="Philadelphia" />
       <footer className="App-footer">
         <a href="https://github.com/lfinkbiner/react-weather">GitHub Repository</a>
       </footer>
--- src/weather.js
+++ src/weather.js
@@ -1,69 +1,69 @@
-import axios from "axios";
 import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
 import "./weather.css";
-import FormattedDate from "./FormattedDate";
 
-export default function Search(props) {
-  const [message, setMessage] = useState(null);
-  const [city, setCity] = useState(null);
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  function showWeather(response) {
-    let date = new Date(response.data.dt*1000);
-    let description = response.data.weather[0].description;
-    let temp = Math.round(response.data.main.temp);
-    let city = response.data.name;
-    let wind = response.data.wind.speed;
-    let humidity = response.data.main.humidity;
-    let iconUrl =`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-    setMessage(<div className="message">
-      <div className="row">
-        <div className="col6">
-          <ul>
-            <li><h2>{city}</h2></li>
-            <li><FormattedDate date={date}/></li>
-            <li className="text-capitalize">{description}</li>
-          </ul>
-        </div>
-        <div className="col 6">
-        </div>
-      </div>
-      <div className="row mt-3">
-      <div className="col 6">
-        <img
-        src={iconUrl}
-        alt = {description}/>
-        <span className="temp">{temp}</span>
-        <span className="unit">°C|F</span>
-        </div>
-    <div className="col 6">
-      <ul>
-        <li>
-          Wind: {wind}km/hr</li>
-          <li>
-            Humidity: {humidity}%</li></ul></div></div></div>);
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=80b63ee33ac52921966d0561121cb9ef&units=metric`;
-    axios.get(url).then(showWeather);
+    search();
   }
 
-  function updateCity(event) {
+  function handleCityChange(event) {
     setCity(event.target.value);
   }
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <div className="row">
-      <div className="col 9"><input type="search" placeholder="type a city..." className="form-control" autoFocus="on" onChange={updateCity} /></div>
-      <div className="col 3"><input type="submit" value="search" className="btn btn-primary"/></div>
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
-    </form>
-  );
-  return (
-    <div className="weather">
-      {form}
-      {message}
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
